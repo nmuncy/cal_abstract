4a4530c1cbf2b543713d0d147b6a06e63b468ebb
Nathan Muncy <nmuncy@fiu.edu>
2021-03-24 14:45:04 -0400

    updated task setup script

--- code/job_taskSetup.py
+++ code/job_taskSetup.py
@@ -1,44 +1,31 @@
 """
 Notes:
 
-    1) This script will make csv files for each block.
-    2) Orients from block_length (number of trials per category),
-        and stim_dict.
-    3) Assumes dimensional stimuli are called "foo_low" and "foo_high"
-        for mapping assignment.
-    4) Assumes stimuli are found in ../Stimuli/foo, will write to ../blocks/BlockX.csv
+    1) Creates subject-specific Block?.csv files in
+        blocks/sub-??
 
-Updates:
-    1) Dropped "Dim" from task
-    2) randomized category assignment
-        writes blocks for e/subj to ../blocks/sub-*/BlockX.csv
-        receives single subj arg
-        removed "dim" syntax (for syntax - v0.1)
+    2) Randomizes theme assignments to each block,
+        response mapping, and stimulus order.
 
-TODO
-    1) update to receive list of subj?
+    3) Currently uses 3 themes for each of 3 blocks.
+
+    4) Orients from cwd for stimuli, blocks
 
 Usage: python job_taskSetup.py 1
 """
+
+# %%
 import os
 import sys
 import random
 import pathlib
 from random import randrange
 import pandas as pd
+import itertools
 
 
+# %%
 def main(subj):
-    """
-    Set up
-
-    Fix1/2 = fixed mapping
-    Cond = conditional mapping
-
-    stim_dict["Block1"]["Fix1"]: "School" = School
-        stimuli will be used for fixed mapping in block1.
-        Also, stimuli will be located in stim_dir/School
-    """
 
     # set paths
     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -50,14 +37,6 @@ def main(subj):
     block_dir = os.path.join(parent_dir, "blocks")
     if not os.path.exists(block_dir):
         os.makedirs(block_dir)
-
-    """
-    Make stim lists per block, for psychopy.
-
-    Will randomize L/R for fixed and dimension mappings (ref coin_toss).
-    Conditional mapping is based off preceding trial.
-    Dataframe will not start with conditional trial.
-    """
 
     # set block length
     block_length = 60
@@ -78,25 +57,38 @@ def main(subj):
     # make random stim_dict
     cat_rand = random.sample(cat_list, len(cat_list))
     stim_dict = {
-        "Block1": {"Fix1": cat_rand[0], "Fix2": cat_rand[1], "Cond": cat_rand[2]},
-        "Block2": {"Fix1": cat_rand[3], "Fix2": cat_rand[4], "Cond": cat_rand[5]},
-        "Block3": {"Fix1": cat_rand[6], "Fix2": cat_rand[7], "Cond": cat_rand[8]},
+        "Block1": {"Fix1": cat_rand[0], "Fix2": cat_rand[1], "Fix3": cat_rand[2]},
+        "Block2": {"Fix1": cat_rand[3], "Fix2": cat_rand[4], "Fix3": cat_rand[5]},
+        "Block3": {"Fix1": cat_rand[6], "Fix2": cat_rand[7], "Fix3": cat_rand[8]},
     }
 
+    # %%
     # make individual blocks
     for block in stim_dict:
         # block = "Block1"
 
-        # randomly assign fixed mapping
-        coin_toss = randrange(2)
-        corr_A = "left" if coin_toss == 1 else "right"
-        corr_B = "left" if corr_A == "right" else "right"
+        # make all permutations of map/type,
+        #   select one at random for map_dict
+        map_options = ["A", "B", "C"]
+        type_options = ["Fix1", "Fix2", "Fix3"]
 
+        all_comb = []
+        h_permut = itertools.permutations(type_options, len(map_options))
+        for comb in h_permut:
+            h_zip = zip(comb, map_options)
+            all_comb.append(dict(list(h_zip)))
+
+        rand_num = randrange(len(all_comb))
+        map_dict = all_comb[rand_num]
+
+        # %%
         # set up block dictionary
-        #   pull random stim of length = block_length
+        #   make list of random stim of length = block_length
+        #   for each type (Fix1-3), attach with mapping in format
+        #   Fix1: [[mapping],[stimuli]]
         block_dict = {
             "Fix1": [
-                [corr_A] * block_length,
+                [map_dict["Fix1"]] * block_length,
                 [
                     os.path.join(stim_rel, stim_dict[block]["Fix1"], x)
                     for x in random.sample(
@@ -106,7 +98,7 @@ def main(subj):
                 ],
             ],
             "Fix2": [
-                [corr_B] * block_length,
+                [map_dict["Fix2"]] * block_length,
                 [
                     os.path.join(stim_rel, stim_dict[block]["Fix2"], x)
                     for x in random.sample(
@@ -115,20 +107,20 @@ def main(subj):
                     )
                 ],
             ],
-            "Cond": [
-                ["Cond"] * block_length,
+            "Fix3": [
+                [map_dict["Fix3"]] * block_length,
                 [
-                    os.path.join(stim_rel, stim_dict[block]["Cond"], x)
+                    os.path.join(stim_rel, stim_dict[block]["Fix3"], x)
                     for x in random.sample(
-                        os.listdir(os.path.join(stim_dir, stim_dict[block]["Cond"])),
+                        os.listdir(os.path.join(stim_dir, stim_dict[block]["Fix3"])),
                         block_length,
                     )
                 ],
             ],
         }
 
-        # make master_dict for pandas df, randomize df,
-        #   don't start with cond type
+        # %%
+        # make master_dict for pandas df, randomize df
         master_dict = {f"{block}Type": [], f"{block}Stim": [], f"{block}Corr": []}
         for key in block_dict.keys():
             for ind in range(0, block_length):
@@ -136,19 +128,43 @@ def main(subj):
                 master_dict[f"{block}Stim"].append(block_dict[key][1][ind])
                 master_dict[f"{block}Corr"].append(block_dict[key][0][ind])
 
+        # %%
+        # make df, randomized df
         df = pd.DataFrame.from_dict(master_dict)
         df_rand = df.sample(frac=1).reset_index(drop=True)
-        while df_rand.at[0, f"{block}Type"] == "Cond":
-            df_rand = df.sample(frac=1).reset_index(drop=True)
 
-        # Set conditional response (opposite from previous)
-        for ind in range(0, df_rand.shape[0]):
-            if df_rand.at[ind, f"{block}Corr"] == "Cond":
-                old_val = df_rand.at[ind - 1, f"{block}Corr"]
-                df_rand.at[ind, f"{block}Corr"] = (
-                    "left" if old_val == "right" else "right"
-                )
+        # make sure same type not presented > 4 times in a row
+        # TODO: instead of regenerating random dfs, could look
+        #   ahead to find new response, and swap?
+        status = True
+        while status:
 
+            # start counting repeats, loop through rows
+            count_rep = 1
+            for ind, row in df_rand.iterrows():
+
+                # skip first row
+                if ind == 0:
+                    continue
+
+                # If Corr == preceding Corr, increase counter.
+                #   reshuffle df, start over if 4 repeat in a row
+                if (
+                    df_rand.loc[ind, f"{block}Corr"]
+                    == df_rand.loc[ind - 1, f"{block}Corr"]
+                ):
+                    count_rep += 1
+                    if count_rep == 4:
+                        df_rand = df.sample(frac=1).reset_index(drop=True)
+                        break
+                else:
+                    # reset counter when Corr changes
+                    count_rep = 1
+
+                if ind == len(df_rand) - 1:
+                    status = False
+
+        # %%
         # write df_rand to csv
         subj_dir = os.path.join(block_dir, f"sub-{subj}")
         if not os.path.exists(subj_dir):
